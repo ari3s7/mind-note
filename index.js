6809fde88b1bbd55af3650ed9abcf3488931c5ce
Abhishek Sharma <abhi761685@gmail.com>
2025-10-14 23:48:28 +0530

    adding input validation using zod

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+import { z } from "zod";
 const app = express();
+
 const PORT = 3000;
+
+
+const signUp = z.object ({
+    username: z.string().min(3).max(10),
+    password: z.string().min(8).max(20).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])/)
+});
 
 app.post("/api/v1/signup", (req,res) => {
     
+    const email = req.body.email;
+    const password = req.body.password;
+
+
 })
 
 
