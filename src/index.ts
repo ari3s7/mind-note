a0024b8e62fc2c419db1537c378cadc3a9112546
Abhishek Sharma <abhi761685@gmail.com>
2025-10-18 14:03:36 +0530

    add signup route

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import z from 'zod';
 import jwt from "jsonwebtoken";
+import { UserModel } from './db.js';
+import  bcrypt  from "bcryptjs";
 const app = express();
 
 const PORT = 3000;
@@ -8,13 +10,32 @@ const PORT = 3000;
 app.use(express.json());
 
 
-const signUp = z.object ({
+
+app.post("/api/v1/signup", async (req,res) => {
+
+
+     const signUp = z.object ({
     username: z.string().min(3).max(10),
     password: z.string().min(8).max(20).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])/)
 });
 
-app.post("/api/v1/signup", (req,res) => {
-    
+
+    const validateData = signUp.safeParse(req.body);
+
+    if (!validateData.success) {
+        res.status(403).json({
+            message: "not correct format"
+        })
+    }
+        const username = req.body.username;
+        const password = req.body.password;
+
+        const hashedPassword = await bcrypt.hash(password, 5);
+
+        const user = await UserModel.create({
+        username: username,
+        password: hashedPassword,
+})
 })
 
 app.post("/api/v1/signin", (req, res) => {
