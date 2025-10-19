6ab2cdfe26b94ee07a8941ef05dfa778e0791a5c
Abhishek Sharma <abhi761685@gmail.com>
2025-10-19 17:13:28 +0530

    add get content route

--- src/index.ts
+++ src/index.ts
@@ -96,12 +96,21 @@ app.post("/api/v1/content", userMiddleware, async (req, res) => {
    })
 })
 
-app.get("/api/v1/content", userMiddleware, (req,res) => {
+app.get("/api/v1/content", userMiddleware, async (req,res) => {
+  // @ts-ignore
+  const userId = req.userId;
+  const content = await ContentModel.find({
+    userId: userId
+  }).populate("userId", "username")
+  res.json({
+    content
+  })
+
 
 })
 
 app.delete("/api/v1/content", (req, res) => {
-
+   
 })
 
 app.post("api/v1/brain/share", (req, res) => {
