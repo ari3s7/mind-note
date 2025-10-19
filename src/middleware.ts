b217adf8880682eba7bf030d57ccd20c8d407a12
Abhishek Sharma <abhi761685@gmail.com>
2025-10-19 16:23:03 +0530

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET;
+
+interface AuthRequest extends Request {
+    userId?: string | number;
+}
+
+export const userMiddleware = (req: AuthRequest, res: Response, next: NextFunction) => {
+    const header = req.headers["authorization"]; 
+    const decoded = jwt.verify(header as string, JWT_SECRET as string)
+    if(decoded) {
+        req.userId = (decoded as any).id;
+        next()
+    } else {
+        res.status(403).json({
+            message: "You are not logged in"
+        })
+    }
+}
