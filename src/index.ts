41a59daa9dbd39202e2d6dfd2a46a7feb8b91425
Abhishek Sharma <abhi761685@gmail.com>
2025-10-19 16:54:42 +0530

    add post content route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import z from 'zod';
 import jwt from "jsonwebtoken";
-import { UserModel } from './db.js';
+import { ContentModel, UserModel } from './db.js';
 import  bcrypt  from "bcryptjs";
 import dotenv from "dotenv";
+import { userMiddleware } from './middleware.js';
 dotenv.config();
 
 const app = express();
@@ -68,7 +69,6 @@ app.post("/api/v1/signin", async (req, res) => {
       return res.status(403).json({ message: "Incorrect credentials" });
     }
 
-    // Generate JWT
     const token = jwt.sign({ id: existingUser._id }, JWT_SECRET!);
 
     res.json({ token });
@@ -78,11 +78,25 @@ app.post("/api/v1/signin", async (req, res) => {
   }
 });
 
-app.post("/api/v1/content", (req, res) => {
+app.post("/api/v1/content", userMiddleware, async (req, res) => {
+   const link = req.body.link;
+   const type = req.body.type;
+   const title = req.body.title;
+   await ContentModel.create ({
+    link,
+    type,
+    title,
+    //@ts-ignore
+    userId: req.userId, 
+    tags: [],
+   })
 
+   return res.json ({
+    message: "Content added"
+   })
 })
 
-app.get("/api/v1/content", (req,res) => {
+app.get("/api/v1/content", userMiddleware, (req,res) => {
 
 })
 
